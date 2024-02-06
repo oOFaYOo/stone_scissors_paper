9ee9ac55f98681c042d28664d2efa3985c1947b4
Nika Ts <nika.tseleva@mail.ru>
2024-02-07 00:10:37 +0400

    decomposed to classes

--- index.js
+++ index.js
@@ -1,64 +1,92 @@
 const _ = require('lodash');
 const crypto = require("crypto");
 
-function io(p,c, ...args){
-    let win = Math.trunc(args.length/2);
-
-    if(p === c) return 'Draw'
-
-    let d = p - c;
-
-    if(d < 0 && -1 * d > win || d > 0 && d <= win){
-        return 'Lose'
-    } else {
-        return "Win"
+class Manager {
+    static values = {
+        '-1': "You lose. Let's try again!",
+        '0': 'Draw',
+        '1': 'You win!',
+        err: 'Error. Make sure you pass an odd number of unique arguments and that the number is greater than or equal to three. Example: rock, scissors, paper.'
     }
-}
 
+    static getAvailableMoves(moves) {
+        moves.forEach((v, i) => {
+            console.log(`${i + 1} - ${v}`)
+        })
+        console.log('0 - exit')
+        console.log('? - help')
+    }
 
-function uuu (p, c, ...args){
-    if(args.length >= 3 && (args.length % 2) !== 0 && _.uniq(args).length === args.length){
+    static _chooseAnswer(max) {
+        const rand = Math.random() * (max + 1 - 0);
+        return Math.floor(rand)
+    }
 
-        let obj = {};
-        args.forEach((v,i)=>{obj[v]=i});
-        let win = Math.trunc(args.length/2);
-
-        if(obj[c] === obj[args[p-1]]) return 'Draw'
-
-        let val = obj[c] + win;
-
-        if(obj[args[p-1]] > val || obj[args[p-1]] < val%args.length){
-            return 'You win!'
+    static start(...args) {
+        const moves = args.slice(2);
+        if (args.length >= 3 && (args.length % 2) !== 0 && _.uniq(args).length === args.length) {
+            const compAnswer = moves[Manager._chooseAnswer(moves.length-1)];
+            const key = KeysService.getKey();
+            console.log(`HMAC: ${KeysService.getHmac(compAnswer, key)}`);
+            console.log('Available moves:');
+            Manager.getAvailableMoves(moves);
         } else {
-            return "You lose. Let's try again!"
+            console.log(Manager.values.err)
         }
+    }
 
-    } else {
-        return 'Error. Make sure you pass an odd number of unique arguments ' +
-            'and that the number is greater than or equal to three. ' +
-            'Example: rock, paper, scissors.'
+}
+
+class Moves {
+    static movesComparer(user, comp, ...args) {
+        const middle = Math.trunc(args.length / 2);
+        const distance = user - comp;
+
+        if (user === comp) return 0; //'Draw'
+
+        if (distance < 0 && -1 * distance > middle || distance > 0 && distance <= middle) {
+            return -1 //'Lose'
+        } else {
+            return 1 //"Win"
+        }
+    }
+}
+
+class KeysService {
+    static getKey() {
+        return crypto.randomBytes(32).toString('hex');
+    }
+
+    static getHmac(move, key) {
+        return crypto.createHmac('sha256', key).update(move).digest("hex");
     }
 }
 
-
-function getKey(str){
-    let buf = crypto.randomBytes(32).toString('hex');
-    return  crypto.createHmac('sha256', buf).update(str).digest("hex");
-}
-
-console.log(uuu('a','e', 'a','b','c','d','e'))
-
-function getTable (...args){
-
-    let rt = {};
-
-    for(let i = 0; i < args.length; i++){
-        rt[args[i]] = {}
-        for(let k = 0; k < args.length; k++){
-            rt[args[i]][[args[k]]] = io(i, k, ...args);
-        }
+class Table {
+    static _values = {
+        '-1': 'Lose',
+        '0': 'Draw',
+        '1': 'Win'
     }
-    return rt;
+
+    static _getTableData(args) {
+        const comparedMoves = {};
+
+        for (let i = 0; i < args.length; i++) {
+            comparedMoves[args[i]] = {}
+            for (let k = 0; k < args.length; k++) {
+                comparedMoves[args[i]][[args[k]]] = Table._values[Moves.movesComparer(i, k, ...args)];
+            }
+        }
+        return comparedMoves;
+    }
+
+    static getTable(...args) {
+        console.table(this._getTableData(args))
+    }
+
 }
 
-console.table(getTable('a','b','c','d','e'))
+// Table.getTable('a','b','c','d','e')
+
+Manager.start(...process.argv)
