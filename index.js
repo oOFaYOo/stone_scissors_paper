3fb105fcb56a1775d0156069ac4acb77c883ac95
Nika Ts <nika.tseleva@mail.ru>
2024-02-07 01:24:14 +0400

    finish main logic

--- index.js
+++ index.js
@@ -1,51 +1,17 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 const _ = require('lodash');
 const crypto = require("crypto");
 
-class Manager {
-    static values = {
-        '-1': "You lose. Let's try again!",
-        '0': 'Draw',
-        '1': 'You win!',
-        err: 'Error. Make sure you pass an odd number of unique arguments and that the number is greater than or equal to three. Example: rock, scissors, paper.'
-    }
-
-    static getAvailableMoves(moves) {
-        moves.forEach((v, i) => {
-            console.log(`${i + 1} - ${v}`)
-        })
-        console.log('0 - exit')
-        console.log('? - help')
-    }
-
-    static _chooseAnswer(max) {
-        const rand = Math.random() * (max + 1 - 0);
-        return Math.floor(rand)
-    }
-
-    static start(...args) {
-        const moves = args.slice(2);
-        if (args.length >= 3 && (args.length % 2) !== 0 && _.uniq(args).length === args.length) {
-            const compAnswer = moves[Manager._chooseAnswer(moves.length-1)];
-            const key = KeysService.getKey();
-            console.log(`HMAC: ${KeysService.getHmac(compAnswer, key)}`);
-            console.log('Available moves:');
-            Manager.getAvailableMoves(moves);
-        } else {
-            console.log(Manager.values.err)
-        }
-    }
-
-}
-
 class Moves {
-    static movesComparer(user, comp, ...args) {
+    static movesComparer(comp, user, ...args) {
         const middle = Math.trunc(args.length / 2);
-        const distance = user - comp;
+        const distance = comp - user;
 
-        if (user === comp) return 0; //'Draw'
+        if (comp === user) return 0; //"Draw"
 
         if (distance < 0 && -1 * distance > middle || distance > 0 && distance <= middle) {
-            return -1 //'Lose'
+            return -1 //"Lose"
         } else {
             return 1 //"Win"
         }
@@ -87,6 +53,55 @@ class Table {
 
 }
 
-// Table.getTable('a','b','c','d','e')
+class Manager {
+    static values = {
+        '-1': "You lose. Let's try again!",
+        '0': 'Draw',
+        '1': 'You win!',
+        err: 'Error. Make sure you pass an odd number of unique arguments and that the number is greater than or equal to three. Example: rock, scissors, paper.'
+    }
 
-Manager.start(...process.argv)
+    static getAvailableMoves(moves) {
+        moves.forEach((v, i) => {
+            console.log(`${i + 1} - ${v}`)
+        })
+        console.log('0 - exit')
+        console.log('? - help')
+    }
+
+    static _chooseComputerAnswer(max) {
+        const rand = Math.random() * (max + 1);
+        return Math.floor(rand)
+    }
+
+    static play(...args) {
+        const moves = args.slice(2);
+        if (args.length >= 3 && (args.length % 2) !== 0 && _.uniq(args).length === args.length) {
+            const compAnswer = Manager._chooseComputerAnswer(moves.length - 1);
+            const key = KeysService.getKey();
+            console.log(`HMAC: ${KeysService.getHmac(moves[compAnswer], key)}`);
+            console.log('Available moves:');
+            Manager.getAvailableMoves(moves);
+            process.stdout.write('Enter your move: ');
+            rl.on('line', (input) => {
+                if (input === '?') {
+                    Table.getTable(...moves);
+                    process.stdout.write('Enter your move: ');
+                } else if (input === '0') {
+                    rl.close();
+                } else {
+                    console.log('Your move: ' + moves[input - 1]);
+                    console.log('Computer move: ' + moves[compAnswer]);
+                    console.log(Manager.values[Moves.movesComparer(compAnswer, input - 1, moves)]);
+                    console.log('HMAC key: ' + key);
+                    rl.close();
+                }
+            });
+        } else {
+            console.error(Manager.values.err);
+            rl.close();
+        }
+    }
+}
+
+Manager.play(...process.argv)
